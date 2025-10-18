8efff9743a83c6ee55391e92197d8969785f2b37
sorahorn <zc21027w@otemae.ac.jp>
2025-10-19 00:38:55 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-
-const listsButton = document.getElementById("lists");
-
-
-const pullDownList = document.getElementById("pull-down");
+const pullDownList = document.getElementById('pull-down');
 
 
-listsButton.addEventListener("click", function() {
+const listsButton = document.getElementById('pullDownButton');
 
-    pullDownList.classList.toggle("hidden");
+listsButton.addEventListener('click', function() {
+  pullDownList.classList.toggle("hidden");
 
-
-    if (pullDownList.classList.contains("hidden")) {
-        console.log("ドロップダウンリストを非表示にしました (hiddenクラスが追加されました)");
-    } else {
-        console.log("ドロップダウンリストを表示しました (hiddenクラスが削除されました)");
-    }
-
+  if (pullDownList.classList.contains("hidden")) {
+    console.log("ドロップダウンリストを非表示にしました (hiddenクラスが追加されました)");
+  } else {
+    console.log("ドロップダウンリストを表示しました (hiddenクラスが削除されました)");
+  }
 });
 
-listsButton.addEventListener("mouseover",function(){
-    console.log("①マウスがプルダウンボタンの上に乗りました(mouseover イベント発火)");
+
+listsButton.addEventListener('mouseover', function() {
+  listsButton.setAttribute('style', 'background-color: blue;');
+  console.log("プルダウンボタンの上にカーソルが乗りました（青色に変更）");
+});
+
+
+listsButton.addEventListener('mouseout', function() {
+  listsButton.removeAttribute('style');
+  console.log("プルダウンボタンからカーソルが外れました（スタイルを削除して元に戻す）");
+});
+
+
+listsButton.addEventListener('click', function() {
+  listsButton.setAttribute('style', 'background-color: green;');
+  console.log("プルダウンボタンがクリックされました（緑色に変更）");
 });
